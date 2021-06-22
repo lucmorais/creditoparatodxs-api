dbcd36701514c66bd5fb00333f17a8f64ca51f67
Luciano Morais <lucianopythonista@gmail.com>
2021-06-22 13:00:52 -0300

    ajuste

--- src/main/kotlin/domain/service/CreditService.kt
+++ src/main/kotlin/domain/service/CreditService.kt
@@ -13,7 +13,7 @@ class CreditService(private val resumeSimulationRepository: ResumeSimulationRepo
     private fun calcValuePortion(cp: CreditPersonal): CreditPersonal? {
         return if (cp.quantityPortion != null && cp.valueCredit != null) {
             var percentage = cp.valueCredit!! * cp.fees
-            cp.valuePortion =  (cp.valueCredit!! / cp.quantityPortion!!) + (percentage/ cp.quantityPortion!!)
+            cp.valuePortion =  (cp.valueCredit!! / cp.quantityPortion!!) + (percentage / cp.quantityPortion!!)
             cp
         }else {
             null
