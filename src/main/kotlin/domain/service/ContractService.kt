bfbbfccf56066d3ec016c8f3cb700a1faefa0b2c
Luciano Morais <lucianopythonista@gmail.com>
2021-06-22 13:49:38 -0300

    ajuste data

--- src/main/kotlin/domain/service/ContractService.kt
+++ src/main/kotlin/domain/service/ContractService.kt
@@ -4,6 +4,7 @@ import domain.Contract
 import domain.ResumeSimulation
 import domain.repository.ContractRepository
 import domain.repository.ResumeSimulationRepository
+import java.text.SimpleDateFormat
 import java.util.*
 
 class ContractService(private val contractRepository: ContractRepository, private val resumeSimulationRepository: ResumeSimulationRepository) {
@@ -20,8 +21,9 @@ class ContractService(private val contractRepository: ContractRepository, privat
 
     fun startDateContract(contract: Contract): Contract? {
         return if (contract.signatureFirstLastName != null) {
-            val date: Date = Date()
-            contract.signatureDate = date.date.toString() + date.month.toString() + date.year.toString()
+            val sdf = SimpleDateFormat("dd/M/yyyy")
+            val currentDate = sdf.format(Date())
+            contract.signatureDate = currentDate.toString()
             addContract(contract)
             contract
         }else {
